603b71cf78860b58fae3cfebbfb0cfba19ae47ab
aproken <aproken@yandex.ru>
2019-12-06 14:15:57 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+var {src, dest, watch} = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 var css = require('gulp-mini-css');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    gulp.watch("./sass/**/*.sass", serveSass);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+};
 
+//Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
 //gulp css min
 gulp.task('css', function(){
     gulp.src(dest+'/*.css')
