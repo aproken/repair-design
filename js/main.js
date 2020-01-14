2e0e739503b90758213becce7a3b686d59864c8e
Наташа <aproken@yandex.ru>
2020-01-14 22:50:48 +0300

    fix: work in response section projects

--- js/main.js
+++ js/main.js
@@ -108,8 +108,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var prev = $('.swiper-button-prev');
     var bullets = $('.swiper-pagination');
 
-    next.css('left', prev.width() + 10 + bullets.width() + 10);
-    bullets.css('left', prev.width() + 10);
+    next.css('left', prev.width() + 10 + bullets.width() + 20);
+    bullets.css('left', prev.width() + 20);
 
     new WOW().init();
 
